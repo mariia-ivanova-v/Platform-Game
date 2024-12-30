5196c85215c112872d427bd93308155f9fcc89a8
Marii <mariiaivanova0911@gmail.com>
2024-12-30 14:07:50 +0100

    Add files via upload

---
+++ levels/level1.js
@@ -0,0 +1,45 @@
+const level1 = new Level(
+    [
+        new GreenSlime(),
+        new GreenSlime(),
+        new GreenSlime(),
+        new BlueSlime(),
+        new BlueSlime(),
+        new BlueSlime(),
+        new BlueSlime(),
+        new Boss()
+    ],
+    [
+        new Background('img/bg/Battleground3/Bright/sky.png',-719,0),
+        new Background('img/bg/Battleground3/Bright/jungle_bg.png',-719,0),
+        new Background('img/bg/Battleground3/Bright/trees&bushes.png',-719,0),
+        new Background('img/bg/Battleground3/Bright/grasses.png',-719,0),
+        new Background('img/bg/Battleground3/Bright/grass&road.png', -719, 0),
+        new Background('img/bg/Battleground3/Bright/lianas.png',-719,0),
+        new Background('img/bg/Battleground3/Bright/fireflys.png',-719,0),
+
+        new Background('img/bg/Battleground3/Bright/sky.png',0,0),
+        new Background('img/bg/Battleground3/Bright/jungle_bg.png',0,0),
+        new Background('img/bg/Battleground3/Bright/trees&bushes.png',0,0),
+        new Background('img/bg/Battleground3/Bright/grasses.png',0,0),
+        new Background('img/bg/Battleground3/Bright/grass&road.png', 0, 0),
+        new Background('img/bg/Battleground3/Bright/lianas.png',0,0),
+        new Background('img/bg/Battleground3/Bright/fireflys.png',0,0),
+
+        new Background('img/bg/Battleground3/Bright/sky.png',719,0),
+        new Background('img/bg/Battleground3/Bright/jungle_bg.png',719,0),
+        new Background('img/bg/Battleground3/Bright/trees&bushes.png',719,0),
+        new Background('img/bg/Battleground3/Bright/grasses.png',719,0),
+        new Background('img/bg/Battleground3/Bright/grass&road.png', 719, 0),
+        new Background('img/bg/Battleground3/Bright/lianas.png',719,0),
+        new Background('img/bg/Battleground3/Bright/fireflys.png',719,0),
+
+        new Background('img/bg/Battleground3/Bright/sky.png',719*2,0),
+        new Background('img/bg/Battleground3/Bright/jungle_bg.png',719*2,0),
+        new Background('img/bg/Battleground3/Bright/trees&bushes.png',719*2,0),
+        new Background('img/bg/Battleground3/Bright/grasses.png',719*2,0),
+        new Background('img/bg/Battleground3/Bright/grass&road.png', 719*2, 0),
+        new Background('img/bg/Battleground3/Bright/lianas.png',719*2,0),
+        new Background('img/bg/Battleground3/Bright/fireflys.png',719*2,0)
+    ]
+);

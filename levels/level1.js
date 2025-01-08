f6e488c4f216526335f8ca9ea0218f5c40c4ffb0
Marii <mariiaivanova0911@gmail.com>
2025-01-08 14:15:54 +0100

    Add files via upload

--- levels/level1.js
+++ levels/level1.js
@@ -6,8 +6,7 @@ const level1 = new Level(
         new BlueSlime(),
         new BlueSlime(),
         new BlueSlime(),
-        new BlueSlime(),
-        new Boss()
+        new BlueSlime()
     ],
     [
         new Background('img/bg/Battleground3/Bright/sky.png',-719,0),
@@ -41,5 +40,29 @@ const level1 = new Level(
         new Background('img/bg/Battleground3/Bright/grass&road.png', 719*2, 0),
         new Background('img/bg/Battleground3/Bright/lianas.png',719*2,0),
         new Background('img/bg/Battleground3/Bright/fireflys.png',719*2,0)
+    ],
+    [
+        new AmmoResources('img/icons/ammo-icon-single.png'),
+        new AmmoResources('img/icons/ammo-icon-single.png'),
+        new AmmoResources('img/icons/ammo-icon-single.png'),
+        new AmmoResources('img/icons/ammo-icon-single.png'),
+        new AmmoResources('img/icons/ammo-icon-single.png'),
+        new AmmoResources('img/icons/ammo-icon-single.png'),
+        new AmmoResources('img/icons/ammo-icon-single.png'),
+        new AmmoResources('img/icons/ammo-icon-single.png')
+    ],
+    [
+        new Coins('img/icons/gold-coin.svg'),
+        new Coins('img/icons/gold-coin.svg'),
+        new Coins('img/icons/gold-coin.svg'),
+        new Coins('img/icons/gold-coin.svg'),
+        new Coins('img/icons/gold-coin.svg'),
+        new Coins('img/icons/gold-coin.svg'),
+        new Coins('img/icons/gold-coin.svg'),
+        new Coins('img/icons/gold-coin.svg')
+    ],
+    [
+        new Beer('img/icons/beer.png'),
+        new Beer('img/icons/beer.png')
     ]
 );
